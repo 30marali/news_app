f0577b52dad9439fc9d7de1a6a37f118e7affc04
Omar <omaropera123@gmail.com>
2024-09-02 17:21:18 +0300

    App init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
+
+
+// 2a52d78427824a4586540a19607ab271
